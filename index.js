11b834c3717ddd5f07b3dfaa697075e804114203
Tassu <git@tassu.me>
2018-06-02 13:52:37 +0300

    heroku i hate you
    Signed-off-by: Tassu <git@tassu.me>

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import server from './src/server/server';
 import db from './src/server/sequelize';
 
 db.sync().then(() => {
-	server.listen(process.env.port || 3000);
-	console.log(`Listening at ${process.env.port || 3000}`);
+	server.listen(process.env.PORT || 3000);
+	console.log(`Listening at ${process.env.PORT || 3000}`);
 });
